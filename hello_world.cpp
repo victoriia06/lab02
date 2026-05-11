56bbe37b12dfa5ccef2c1698e894e452ce8696ed
victoriia06 <k.kim221b@gmail.com>
2026-05-12 00:12:19 +0300

    fix code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main()
 {
-    string name;
+    std::string name;
 
-    cout << "Enter your name: ";
-    cin >> name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
 
-    cout << "Hello world from " << name << endl;
+    std::cout << "Hello world from " << name << std::endl;
 
     return 0;
 }
