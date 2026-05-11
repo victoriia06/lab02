59ac1a1c2e2581f8d2e8ef288a326742c05d0841
victoriia06 <k.kim221b@gmail.com>
2026-05-12 00:13:40 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,11 +3,14 @@
 
 int main()
 {
+    // Variable for storing user name
     std::string name;
 
+    // Ask user to enter name
     std::cout << "Enter your name: ";
     std::cin >> name;
 
+    // Print greeting message
     std::cout << "Hello world from " << name << std::endl;
 
     return 0;
